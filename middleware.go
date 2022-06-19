c203d7492ba53fabec8f0953c81f130ab46a4ea8
Roy <oisroy233@gmail.com>
2022-06-19 20:08:46 +0800

    新增静态资源缓存控制

--- middleware.go
+++ middleware.go
@@ -139,3 +139,23 @@ func csrfVerify(token string, auth *JWTStruct) (err error) {
 func csrfSetCookie(c *gin.Context, token string) {
 	c.SetCookie("CSRF-TOKEN", token, 1*60*60, "/", "", true, false)
 }
+
+func CacheMiddleware(c *gin.Context) {
+	if c.Request.Method != "GET" {
+		c.Next()
+		return
+	}
+	ext := []string{"gif", "jpg", "png", "js", "css", "js.map"}
+	has := false
+	for _, s := range ext {
+		if strings.HasSuffix(c.Request.URL.Path, s) == true {
+			c.Header("cache-control", "max-age=43201")
+			has = true
+			break
+		}
+	}
+	if has == false {
+		c.Header("cache-control", "no-cache")
+	}
+	c.Next()
+}
